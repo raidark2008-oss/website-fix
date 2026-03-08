4bf64c1964c28526e690f0b087c768e4f04fb008
User <user@example.com>
2026-03-08 22:01:56 +0700

    file index.html pada folder Update 1

--- infor.html/infor.py
+++
@@ -1,9 +0,0 @@
-print ("masukan angka buat pertanyaan angka favoritmu)
-       input (a)
-       print (\n * 1000)
-         print ("tebak angka favorit yang nu;lis"))
-input (b)
-if a==b:
-    print ("benar")
-else:
-    print ("salah")
